ace45d22c20a6abf7c294c8f92167cfd0a572e1c
Masud Chy <masud3e.095@gmail.com>
2021-06-07 16:52:56 +0600

    2nd commit

---
+++ src/components/TeamRecord/TeamRecord.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './TeamRecord.css';
+
+const TeamRecord = () => {
+    return (
+        <div className="team-container">
+            <div className="player-container">
+                <h2>Here are the complete data of the world's best players. You can select the best 11 for your team. </h2>
+            </div>
+            <div className="budget-container">
+                <h3>This is the section of budget.</h3>
+            </div>
+        </div>
+    );
+};
+
+export default TeamRecord;
